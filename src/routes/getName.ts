4f69ac42dc630ee5285426e8c21e7228e9c79e9e
heleno <marioanimes09@gmail.com>
2024-04-25 11:14:33 -0300

    getName

--- src/routes/getName.ts
+++ src/routes/getName.ts
@@ -5,7 +5,7 @@ export type GetNameResponse = [
     USR_CODIGO: string
   },
 ]
-const getName = async (user: string) => {
+const getName = async (user: string | undefined) => {
   const response = await api.get<GetNameResponse>(`/api/name/${user}`)
   return response.data
 }
